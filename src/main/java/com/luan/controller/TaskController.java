0a9584ee9b75f4a09625281ae130ba2e6e41617a
Luan Moraes <luansilvamoraess@gmail.com>
2024-03-20 01:01:41 -0300

    Criação dos metodos Procurar por id, Atualizar por id e Deletar por id

--- src/main/java/com/luan/controller/TaskController.java
+++ src/main/java/com/luan/controller/TaskController.java
@@ -15,18 +15,58 @@ import java.util.List;
 public class TaskController {
 
     private final TaskRepository taskRepository;
-    @GetMapping("/list-tasks")
+    @GetMapping
     public @ResponseBody List<Task> list(){
         return taskRepository.findAll();
     }
 
-    @PostMapping("/register-task")
+    @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Task create(@RequestBody Task task){
         return taskRepository.save(task);
         //return ResponseEntity.status(HttpStatus.CREATED)
         //        .body(taskRepository.save(task));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> findById(@PathVariable Long id){
+        return taskRepository.findById(id)
+                .map(itemFound -> ResponseEntity.ok().body(itemFound))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    /**
+     * Método para atualizar uma tarefa existente pelo ID.
+     *
+     * @param id    O ID da tarefa a ser atualizada.
+     * @param task  Os novos dados da tarefa a serem atualizados.
+     * @return      ResponseEntity contendo a tarefa atualizada no corpo da resposta.
+     *              Retorna HTTP 200 OK se a tarefa é atualizada com sucesso.
+     *              Retorna HTTP 404 Not Found se a tarefa com o ID fornecido não é encontrada.
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> update(@PathVariable Long id, @RequestBody Task task){
+        return taskRepository.findById(id)
+                .map(itemFound -> {
+                    itemFound.setTitle(task.getTitle());
+                    itemFound.setDescription(task.getDescription());
+                    Task updated = taskRepository.save(itemFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        return taskRepository.findById(id)
+                .map(itemFound -> {
+                    taskRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
 
 }
 
